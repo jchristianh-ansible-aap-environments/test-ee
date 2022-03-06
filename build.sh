ecc77c72bfc659fb7d036f995b9700570b8d9a9b
Chris Hammer <chris.hammer@redhat.com>
2022-03-06 01:14:05 -0500

    updates to everything ;)

--- build.sh
+++ build.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+# BUILD & PUSH
+##############
 ansible-builder create -f test-environment.yml
 podman build -f context/Containerfile -t test-ee
-podman tag localhost/test-ee:latest conjur.thezengarden.net/test-ee:`date +%y%m`
-podman push conjur.thezengarden.net/test-ee:`date +%y%m`
+podman tag localhost/test-ee:latest conjur.thezengarden.net/test-ee:1.0.`date +%m%d`
+podman push conjur.thezengarden.net/test-ee:1.0.`date +%m%d`
+
+
+# CLEANUP
+#########
+podman image prune -f
+rm -rf context
